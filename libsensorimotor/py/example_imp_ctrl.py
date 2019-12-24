27b9e869eb200e0ad348dff8322b09fc02fd026a
wally <wallychung@aibrain.co.kr>
2019-12-24 14:36:10 +0900

    remove

---
+++ libsensorimotor/py/example_imp_ctrl.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+"""
+   +---------------------------------------------------------+
+   | Warning: This example demonstrates the impulse control  |
+   | method to be used with solenoids and should NOT be used |
+   | for servo motors with end-stops. ~~~~~~~~~~~~~~~~~~~~~~ |
+   +---------------------------------------------------------+
+"""
+
+from src.sensorimotor import Sensorimotor
+from time import sleep
+
+def print_position(data):
+    print(''.join('{0: .2f} '.format(k) for k in data))
+
+
+tst0 = [ ([0.0, 0.6, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.6, 0.0, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.0, 0.6, 0.0], 0.10)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.0, 0.6, 0.0], 0.10)
+       , ([0.6, 0.0, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       ]
+
+tst1 = [ ([0.0, 0.6, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.6, 0.0, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       ]
+
+
+def main():
+    motors = Sensorimotor(number_of_motors = 3, verbose = False)
+
+    try:
+        # checking for motors
+        N = motors.ping()
+        print("Found {0} sensorimotors.".format(N))
+        sleep(1.0)
+
+        #TODO: set this according to your supply voltage and desired max. motor speed
+        #motors.set_voltage_limit([0.25, 0.25, 0.25])
+
+        # starting motorcord
+        motors.start()
+
+        beat = tst0
+
+        while(True):
+            # USER CODE HERE BEGIN
+
+            print("tak")
+            for b in beat:
+                motors.apply_impulse(b[0])
+                sleep(b[1]*1.2)
+
+            # USER CODE HERE END
+
+
+    except (KeyboardInterrupt, SystemExit):
+
+        # stopping motor cord
+        print("\rAborted, stopping motors")
+        motors.stop()
+
+    except:
+        # Script crashed?
+        print("\rException thrown, stopping motors")
+        motors.stop()
+
+        raise
+
+
+    print("____\nDONE.")
+
+
+if __name__ == "__main__":
+    main()
+
